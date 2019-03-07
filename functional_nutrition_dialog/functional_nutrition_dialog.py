638137d313a97efdfae905c1abe429a6fc605c95
dima <dmitry@retailnext.net>
2019-03-07 17:18:29 +0300

    Fixed relative dates

--- functional_nutrition_dialog/functional_nutrition_dialog.py
+++ functional_nutrition_dialog/functional_nutrition_dialog.py
@@ -157,29 +157,29 @@ def adjust_relative_dates(
         *,
         initial_date: datetime.datetime,
         yandex_dict: dict) -> datetime.datetime:
-    relative_year = yandex_dict['year_is_relative'] \
+    relative_year = yandex_dict['year'] \
         if ('year_is_relative' in yandex_dict and
-            yandex_dict['year'] is True) else 0
+            yandex_dict['year_is_relative'] is True) else 0
 
-    relative_month = yandex_dict['month_is_relative'] \
+    relative_month = yandex_dict['month'] \
         if ('month_is_relative' in yandex_dict and
-            yandex_dict['month'] is True) else 0
+            yandex_dict['month_is_relative'] is True) else 0
 
-    relative_day = yandex_dict['day_is_relative'] \
+    relative_day = yandex_dict['day'] \
         if ('day_is_relative' in yandex_dict and
-            yandex_dict['day'] is True) else 0
+            yandex_dict['day_is_relative'] is True) else 0
 
-    relative_hour = yandex_dict['hour_is_relative'] \
+    relative_hour = yandex_dict['hour'] \
         if ('hour_is_relative' in yandex_dict and
-            yandex_dict['hour'] is True) else 0
+            yandex_dict['hour_is_relative'] is True) else 0
 
-    relative_minute = yandex_dict['minute_is_relative'] \
+    relative_minute = yandex_dict['minute'] \
         if ('minute_is_relative' in yandex_dict and
-            yandex_dict['minute'] is True) else 0
+            yandex_dict['minute_is_relative'] is True) else 0
 
-    relative_second = yandex_dict['second_is_relative'] \
+    relative_second = yandex_dict['second'] \
         if ('second_is_relative' in yandex_dict and
-            yandex_dict['second'] is True) else 0
+            yandex_dict['second_is_relative'] is True) else 0
     return initial_date + dateutil.relativedelta.relativedelta(
             years=relative_year,
             months=relative_month,
@@ -270,7 +270,7 @@ def respond_with_context(
         last_datetime_entity = [entity for entity in request.entities if
                                 entity['type'] == "YANDEX.DATETIME"][-1]
         absolute_date = transform_yandex_datetime_value_to_datetime(
-                yandex_datetime_value_dict=last_datetime_entity)
+                yandex_datetime_value_dict=last_datetime_entity['value'])
         print(absolute_date)
         pass
         # return response_with_context_when_date_in_request(
