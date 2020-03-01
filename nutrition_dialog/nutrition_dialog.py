0dd45529355b600b7b6c34fea839900f80ee86f1
kreodont <dimasanov@gmail.com>
2020-03-01 23:26:11 +0300

    More fixes from user request

--- nutrition_dialog/nutrition_dialog.py
+++ nutrition_dialog/nutrition_dialog.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
 
     result = nutrition_dialog(
         event=mockers.mock_incoming_event(
-            phrase='ведро чипсов',
+            phrase='что уже записано',
             timezone='UTC+3',
             write_to_food_cache=False,
 
